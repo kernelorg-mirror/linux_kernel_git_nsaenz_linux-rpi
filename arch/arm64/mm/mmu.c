d0b82f181f1b019719c9f96cfedd3d9f0952e44f
Catalin Marinas <catalin.marinas@arm.com>
2020-11-19 15:13:18 +0100

    arm64: mm: Avoid block mappings if kexec is enabled
    By the time map_mem() is called we haven't had yet the opportunity to
    parse the firmware description (DT/ACPI). Parsing it depends on having
    the linear mapping ready. On the other hand, we can't really take
    informed decisions on crashkernel's placement before getting the
    system's DMA addressing constraints, which comes from the firmware
    description.
    
    This can be solved by moving crashkernel's initialization after the
    firmware description parsing. With the drawback that crashkernel depends
    on its memory being mapped with page granularity. This has to happen in
    mem_map(), which, as stated above, is a prerequisite to firmware
    parsing.
    
    To get around this, force all the mappings to page granularity when
    kexec/crashkernel is enabled. Thus allowing us to reserve the
    crashkernel memory later in the boot process, without breaking its
    functionality.
    
    Note that 'rodata_full=true' has been the default behaviour for a while
    so this is unlikely to change anything on most setups.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    [nsj: wrote patch description]
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -477,7 +477,8 @@ static void __init map_mem(pgd_t *pgdp)
 	int flags = 0;
 	u64 i;
 
-	if (rodata_full || debug_pagealloc_enabled())
+	if (rodata_full || debug_pagealloc_enabled() ||
+	    IS_ENABLED(CONFIG_KEXEC_CORE))
 		flags = NO_BLOCK_MAPPINGS | NO_CONT_MAPPINGS;
 
 	/*
@@ -487,11 +488,6 @@ static void __init map_mem(pgd_t *pgdp)
 	 * the following for-loop
 	 */
 	memblock_mark_nomap(kernel_start, kernel_end - kernel_start);
-#ifdef CONFIG_KEXEC_CORE
-	if (crashk_res.end)
-		memblock_mark_nomap(crashk_res.start,
-				    resource_size(&crashk_res));
-#endif
 
 	/* map all the memory banks */
 	for_each_mem_range(i, &start, &end) {
@@ -518,21 +514,6 @@ static void __init map_mem(pgd_t *pgdp)
 	__map_memblock(pgdp, kernel_start, kernel_end,
 		       PAGE_KERNEL, NO_CONT_MAPPINGS);
 	memblock_clear_nomap(kernel_start, kernel_end - kernel_start);
-
-#ifdef CONFIG_KEXEC_CORE
-	/*
-	 * Use page-level mappings here so that we can shrink the region
-	 * in page granularity and put back unused memory to buddy system
-	 * through /sys/kernel/kexec_crash_size interface.
-	 */
-	if (crashk_res.end) {
-		__map_memblock(pgdp, crashk_res.start, crashk_res.end + 1,
-			       PAGE_KERNEL,
-			       NO_BLOCK_MAPPINGS | NO_CONT_MAPPINGS);
-		memblock_clear_nomap(crashk_res.start,
-				     resource_size(&crashk_res));
-	}
-#endif
 }
 
 void mark_rodata_ro(void)
